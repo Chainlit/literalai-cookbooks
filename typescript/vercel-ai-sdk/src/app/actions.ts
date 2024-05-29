03b564600a1f85f4d07258202829546d0701e4eb
Brendan D <Granipouss@gmail.com>
2024-05-29 13:28:11 +0200

    feat: add Vercel AI SDK example

---
+++ typescript/vercel-ai-sdk/src/app/actions.ts
@@ -0,0 +1,40 @@
+"use server";
+
+import { streamText } from "ai";
+import { openai } from "@ai-sdk/openai";
+import { createStreamableValue } from "ai/rsc";
+
+import { LiteralClient } from "@literalai/client";
+
+const literalClient = new LiteralClient();
+const streamTextMonitored =
+  literalClient.instrumentation.vercel.instrument(streamText);
+
+export interface Message {
+  role: "user" | "assistant";
+  content: string;
+}
+
+export async function continueConversation(history: Message[]) {
+  const stream = createStreamableValue();
+
+  (async () => {
+    const { textStream } = await streamTextMonitored({
+      model: openai("gpt-3.5-turbo"),
+      system:
+        "You are a dude that doesn't drop character until the DVD commentary.",
+      messages: history,
+    });
+
+    for await (const text of textStream) {
+      stream.update(text);
+    }
+
+    stream.done();
+  })();
+
+  return {
+    messages: history,
+    newMessage: stream.value,
+  };
+}
