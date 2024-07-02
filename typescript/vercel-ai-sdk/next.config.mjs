83a64cddfd0d343cc9a3d6b958b3caf19bf373ca
Damien BUTY <dam.buty@gmail.com>
2024-07-02 17:27:18 +0200

    fix: make sure all TS cookbooks work with the latest version

--- typescript/vercel-ai-sdk/next.config.mjs
+++ typescript/vercel-ai-sdk/next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "standalone",
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // Fixes npm packages that depend on `fs` module
+      config.resolve.fallback.fs = false;
+    }
+
+    // This is necessary because of a binary dependency in openai
+    config.module.rules.push({
+      test: /\.node$/,
+      use: "node-loader",
+    });
+
+    return config;
+  },
+};
 
 export default nextConfig;
